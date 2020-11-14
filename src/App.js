1f2c336dad959d439f38e244b6531ef3f1ed35dc
Matthew Henderson <mattcharlesh@gmail.com>
2020-11-14 15:33:16 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import './App.css';
+import Search from './Search'
+const App = () => {
+  const [users, setUsers] = useState(null);
+  let apiURL = 'https://randomuser.me/api/?results=10';
+
+  const fetchUsers = () =>
+    fetch(apiURL)
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data.results);
+        console.log(users);
+      });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='header'>
+        <h1>Random Users</h1>
+
+        <Search/>
+        
+        <button className='fetch-button' onClick={fetchUsers}>
+          Fetch Data
+        </button>
+      </div>
+
+      <div className='users'>
+        {users &&
+          users.map((user, index) => {
+            return (
+              <div className='user' key={index}>
+                <img className="profile-img" src={user.picture.medium} alt='user' />
+                <h2>{user.name.first + ' ' + user.name.last}</h2>
+                <div className='details'>
+                  <p><strong>Email: </strong> {user.email}</p>
+                  <p><strong>Phone:</strong> {user.phone}</p>
+                  <p><strong>Birth Year:</strong> {'' + new Date(user.dob.date).getFullYear()}
+                  </p>
+                  <p><strong>Location: </strong>
+                    {` ${user.location.city}, ${user.location.state}`} </p>
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div>
+      <textarea>Insert Text Here</textarea>
+    </div>
+  )
+}
+
+export default Search
